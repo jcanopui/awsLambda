8c2b293787822ccbace3eaa54c5b300a2d09a363
Xavi Ramos <xavier.ramos.sole@ameu8.com>
2016-05-02 17:17:42 +0200

    Add entities to lambda

--- lambda-confirm/src/main/java/com/zurich/Confirm.java
+++ lambda-confirm/src/main/java/com/zurich/Confirm.java
@@ -4,86 +4,29 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.everis.push.services.entities.NotificationStatusEntity;
+import com.zurich.entities.NotificationStatusEntity;
+import com.zurich.entities.RequestClass;
+import com.zurich.entities.ResponseClass;
 
 public class Confirm {
 
-
 	static AmazonDynamoDBClient dynamoClient = new AmazonDynamoDBClient().withRegion(Regions.US_EAST_1);
-	
-	/*
-	 * Request Class
-	 */
-	public static class RequestClass {
-		
-		long notificationId;
-		
-		public long getNotificationId() {
-			return notificationId;
-		}
-
-		public void setNotificationId(long notificationId) {
-			this.notificationId = notificationId;
-		}
-
-		public RequestClass(long notificationId) {
-			this.notificationId = notificationId;
-		}
-
-		public RequestClass() {}
-	}
-	
-	/*
-	 * Response Class
-	 */
-	public static class ResponseClass {
-		
-		boolean result;
-		
-		public boolean isResult() {
-			return result;
-		}
-
-		public void setResult(boolean result) {
-			this.result = result;
-		}
-
-		public ResponseClass(boolean result) {
-			this.result = result;
-		}
-
-		public ResponseClass() {}
-	}
 	
 	/*
 	 * Handler Register
 	 */
 	public static ResponseClass myHandler(RequestClass request, Context context) {
-		//TODO what is the value that this method must be return?
 		
-		/*boolean result = */saveToDynamoDB(request.getNotificationId());
+		saveToDynamoDB(request.getNotificationId());
 		
-		return new ResponseClass(true/*result*/);
+		return new ResponseClass(true);
 	}
 	
 	private static void saveToDynamoDB(long notificationId) {
-		
+
 		DynamoDBMapper mapper = new DynamoDBMapper(dynamoClient);
 		NotificationStatusEntity notificationStatus = 
 				new NotificationStatusEntity(notificationId, NotificationStatusEntity.ACK);
 		mapper.save(notificationStatus);
-/*		
-		AmazonDynamoDBClient dynamoClient = new AmazonDynamoDBClient().withRegion(Regions.US_EAST_1);
-		DynamoDB dynamoDB = new DynamoDB(dynamoClient);
-		Table table = dynamoDB.getTable("NOTIFICATIONS_STATUS");
-
-		Item item = new Item()
-				.withPrimaryKey("notificationId",notificationId)
-				.withBoolean("notificationStatus", true);
-		
-		PutItemOutcome outcome = table.putItem(item);
-		
-		return (outcome!=null);
-*/
 	}
 }
